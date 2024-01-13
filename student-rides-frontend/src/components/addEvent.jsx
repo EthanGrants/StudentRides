af54dd0c85328db5a187db402becf37f75c4ff12
Sophia Peckner <sophiapeckner@gmail.com>
2024-01-13 13:23:51 -0800

    create new events and add to database

--- student-rides-frontend/src/components/addEvent.jsx
+++ student-rides-frontend/src/components/addEvent.jsx
@@ -1,37 +1,113 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { firestore } from "../firebase";
-import { addDoc, collection } from "@firebase/firestore";
+import { collection, query, getDocs, addDoc } from "@firebase/firestore";
+import DateTimePicker from "react-datetime-picker";
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
-export default function Home() {
-    const dateRef = useRef();           // date of departure
-    const destinationRef = useRef();    // destination from UCSB
-    const seatsRef = useRef();          // open seats
+export default function AddEvent() {
+    // ~fetching Firestore data~
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        async function fetchEvents() {
+            try {
+                const q = query(collection(firestore, "events"));
+                const querySnapshot = await getDocs(q);
+                const currentEvents = querySnapshot.docs.map(doc => doc.data());    
+                setEvents(currentEvents);
+            } catch (err) {
+                console.log(err);
+            } 
+        }
+        fetchEvents();
+    }, []);
+
+    // ~add events functionality~
+    const event = {
+        startLocation: "",      // default is UCSB
+        endLocation: "",        // ride destination
+        departure: new Date(),  // departure date and time
+        seats: 0,               // num seats available in the car
+        cost: 0,                // cost of ride
+        contact: "",            // phone number
+    }
+    
+    // newEvents initialized to a "default" data entry
+    const [newEvent, setNewEvent] = useState(event);
+    // allEvents initialized to the current data in Firestore
+    const [allEvents, setAllEvents] = useState(events);
+    // for DateTimePicker
+    const[value, setValue] = useState(new Date());
     const ref = collection(firestore, "events")
 
-    const handleSave = async(e) => {
+    // add document to collection
+    const handleAddEvent = (e) => {
         e.preventDefault();
-        
-        let data = {
-            date: dateRef.current.value,
-            destination: destinationRef.current.value,
-            seats: seatsRef.current.value,
-        }
+        // spreads current events and pushes new event to allEvents
+        setAllEvents([...allEvents, newEvent]);
         
         try {
-            addDoc(ref, data);
+            // add new event to collection
+            addDoc(ref, newEvent);
         } catch(e) {
             console.log(e);
         }
     }
+
     return (
         <div>
-            <form onSubmit={handleSave}>
-                <label>Post a ride!</label>
-                <input type="date" ref={dateRef} />
-                <input type="text" ref={destinationRef} placeholder="i.e. San Luis Opisbo" />
-                <input type="number" ref={seatsRef} placeholder="i.e. 4" />
-                <button type="submit">Save</button>
-            </form>
+            <h2>Add New Event</h2>
+            <div>
+                <input 
+                    type="text"
+                    placeholder="Start Location"
+                    style={{width:"20%", marginRight:"10px"}}
+                    value={newEvent.startLocation}
+                    onChange={(e) => setNewEvent({...newEvent, startLocation: e.target.value})}
+                />
+                <input 
+                    type="text"
+                    placeholder="End Location"
+                    style={{width:"20%", marginRight:"10px"}}
+                    value={newEvent.endLocation}
+                    onChange={(e) => setNewEvent({...newEvent, endLocation: e.target.value})}
+                />
+                <DateTimePicker
+                    placeholderText="Departure"
+                    style={{marginRight: "10px"}}
+                    selected={newEvent.departure}
+                    value={value}
+                    onChange={(departure) => setNewEvent({...newEvent, departure})}
+                />
+                <input 
+                    type="num"
+                    placeholder="Seats"
+                    style={{width:"20%", marginRight:"10px"}}
+                    value={newEvent.seats}
+                    onChange={(e) => setNewEvent({...newEvent, seats: e.target.value})}
+                />
+                <input 
+                    type="num"
+                    placeholder="Cost"
+                    style={{width:"20%", marginRight:"10px"}}
+                    value={newEvent.cost}
+                    onChange={(e) => setNewEvent({...newEvent, cost: e.target.value})}
+                />
+                <input 
+                    type="tel"
+                    placeholder="Contact"
+                    style={{width:"20%", marginRight:"10px"}}
+                    value={newEvent.contact}
+                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    onChange={(e) => setNewEvent({...newEvent, contact: e.target.value})}
+                />
+                <button 
+                    style={{marginTop: "10px"}}
+                    onClick={handleAddEvent}>
+                    Add Event
+                </button>
+            </div>
         </div>
     )
 }

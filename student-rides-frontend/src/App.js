7362f197fecf066aba48311fbe19de83f6a649e9
Sophia Peckner <sophiapeckner@gmail.com>
2024-01-13 14:49:15 -0800

    define props and add AddEvent section

--- student-rides-frontend/src/App.js
+++ student-rides-frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import AddEvent from "./components/addEvent"
 import RidesCalendar from './components/calendar';
+import { useState } from "react";
 
 function App() {
+  // allEvents initialized to the current data in Firestore
+  const [allEvents, setAllEvents] = useState();
+
   return (
     <div className="App">
-      <RidesCalendar />
+      <AddEvent allEvents={allEvents} setAllEvents={setAllEvents}/>
+      <RidesCalendar allEvents={allEvents} setAllEvents={setAllEvents}/>
     </div>
   );
 }

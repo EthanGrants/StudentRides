23aa5bff6fd30a0475a987cc28ddfd36f7b99c89
Sophia Peckner <sophiapeckner@gmail.com>
2024-01-13 20:22:09 -0800

    move fetch data functionality here

--- student-rides-frontend/src/App.js
+++ student-rides-frontend/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-import AuthDetails from "./components/AuthDetails"
 import AddEvent from "./components/addEvent"
 import RidesCalendar from './components/calendar';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signOut } from 'firebase/auth';
-import { auth } from './firebase';
+import { auth, firestore } from './firebase';
+import { collection, query, getDocs } from "@firebase/firestore";
 
 function App() {
   // allEvents initialized to the current data in Firestore
@@ -20,6 +20,28 @@ function App() {
       console.log("Sign out was successful")
     }).catch(error => console.log(error))
   }
+  
+  // ~fetching Firestore data~
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const q = query(collection(firestore, "events"));
+        const querySnapshot = await getDocs(q);
+        const currentEvents = querySnapshot.docs.map((doc) => doc.data());
+        for (let i = 0; i < currentEvents.length; i++) {
+          currentEvents[i].start = new Date(
+            currentEvents[i].start.seconds * 1000
+          );
+          currentEvents[i].end = new Date(currentEvents[i].end.seconds * 1000);
+        }
+        setAllEvents(currentEvents);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchEvents();
+  }, [setAllEvents]);
+
   return (
     <div className="App">
       <h1>Student Rides</h1>
